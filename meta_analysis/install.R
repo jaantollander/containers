b499fe33de0d0367970b0ade1608fa0027099654
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-01-31 07:56:40 +0200

    files for installing R packages

---
+++ meta_analysis/install.R
@@ -0,0 +1,5 @@
+install.packages('pacman', repos='http://cran.us.r-project.org')
+install.packages('devtools')
+list.p=c("dplyr","Matrix","future","pheatmap","ggplot2","optparse","hdf5r");pacman::p_load(list.p, character.only = TRUE)
+install.packages('rmatio')
+install.packages('XML')
