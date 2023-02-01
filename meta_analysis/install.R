38c9aa736e8a6f97036f91a33987781d21ccfeb5
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-02-01 15:21:22 +0200

    install R packages from script

--- meta_analysis/install.R
+++ meta_analysis/install.R
@@ -1,5 +1,7 @@
+#!/usr/bin/env Rscript --vanilla
 install.packages('pacman', repos='http://cran.us.r-project.org')
 install.packages('devtools')
-list.p=c("dplyr","Matrix","future","pheatmap","ggplot2","optparse","hdf5r");pacman::p_load(list.p, character.only = TRUE)
+list.p=c("dplyr","Matrix","future","pheatmap","ggplot2","optparse","hdf5r")
+pacman::p_load(list.p, character.only = TRUE)
 install.packages('rmatio')
 install.packages('XML')
