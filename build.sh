52e92a5dbdc0e6753ce6761adf0b88e1d4a0102e
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-02-03 10:16:11 +0200

    build script

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+IMAGES_DIR="images"
 
 build() {
     NAME=$1
     TAG=$2
+    DATE=$(date -u +"%Y-%m-%d_%H:%M:%S")
+    LOG_DIR="$NAME/.logs/$DATE"
+    mkdir -p "$LOG_DIR"
     sudo docker build --tag "$NAME:$TAG" "$NAME" \
-        1> >(tee "$NAME/stdout.log") \
-        2> >(tee "$NAME/stderr.log" >&2)
+        1> >(tee "$LOG_DIR/stdout.log") \
+        2> >(tee "$LOG_DIR/stderr.log" >&2)
 }
 
 push() {
@@ -20,14 +24,15 @@ pull() {
     NAME=$1
     TAG=$2
     OWNER=$3
-    apptainer pull "$NAME.sif" "docker://ghcr.io/$OWNER/$NAME:$TAG"
+    mkdir -p "$IMAGES_DIR"
+    apptainer pull "$IMAGES_DIR/$NAME.sif" "docker://ghcr.io/$OWNER/$NAME:$TAG"
 }
 
 # Pass the arguments
 case $1 in
     (build) build "$2" "${3:-"latest"}" ;;
     (push) push "$2" "$3" "$4" ;;
-    (pull) pull"$2" "$3" "$4" ;;
+    (pull) pull "$2" "$3" "$4" ;;
     (*) exit 1 ;;
 esac
 
